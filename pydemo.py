a8166f06d0007fa6f79569bf3cf379bd73bfeae0
“ElakkiyaVijayababu” <ennlucky@gmail.com>
2021-12-11 12:56:43 +0000

    git second demo- first commit-Elakkiya

---
+++ pydemo.py
@@ -0,0 +1,27 @@
+'''a=10
+b=2.00
+c="leena"
+d=True
+print(a,type(a)) 
+print(b,type(b)) 
+print(c,type(c)) 
+print(d,type(b))
+'''
+print("enter the first number:")
+a=input()
+print("enter the second number:")
+b=input()
+c=float(float(a)-float(b))
+print("sum of both numbers",float(c))
+
+
+
+
+
+
+
+
+
+
+
+
